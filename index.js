cfed478ee3401c3ed6ac80783c6c44a7c878aaab
Kriti Mahajan <134840866+Krmjn09@users.noreply.github.com>
2024-01-24 02:44:11 +0000

    routes initialised

--- index.js
+++ index.js
@@ -1,18 +1,39 @@
 // first importing express
 const express = require("express");
-
+const {users}= require("./data/users.json");
 const app = express();
 
-const PORT = 8081;
+const port = 3000;
 
 app.use(express.json()); // we are uing the application only in json format
 
-app.get("/api/posts", (req, res) => {
+
+app.get("/", (req, res) => {
   res.status(200).json({
     message: "server is up and running :-)",
+    data: "hey",
+  });
+});
+/*
+Route: /users
+Method: GET
+Description: Get all users
+Access: Public
+Parameter: none
+*/
+
+app.get("/users",(req,res)=>{
+  res.status(200).json({
+    success:true,
+    data: users,
   });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running at port ${PORT}`);
+app.get("*",(req,res)=>{
+  res.status(404).json({
+    message:"This route does not exists"
+  });
+})
+app.listen(port, () => {
+  console.log(`Server is running at port ${port}`);
 });
